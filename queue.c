d8b16c21837a729b53a96323848be5bb79c74bd9
YuJiaHao <goot121433@gmail.com>
2022-02-23 20:26:40 +0800

    Implement q_reverse()
    Swap prev and next every node

--- queue.c
+++ queue.c
@@ -241,7 +241,18 @@ void q_swap(struct list_head *head)
  * (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
  * It should rearrange the existing ones.
  */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head))
+        return;
+    struct list_head *li = head;
+    do {
+        struct list_head *tmp = li->next;
+        li->next = li->prev;
+        li->prev = tmp;
+        li = tmp;
+    } while (li != head);
+}
 
 /*
  * Sort elements of queue in ascending order
